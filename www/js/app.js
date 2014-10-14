af21bac7da17de50e450c1c5d5171033e2ddadc0
Matt Keef <mkeef1@gmail.com>
2014-10-14 14:15:36 -0500

    initial

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+  angular.module('mk-calculator', ['ionic'])
+
+  .run(function($ionicPlatform){
+  $ionicPlatform.ready(function(){
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard){
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar){
+      StatusBar.styleDefault();
+        }
+      });
+  });
+})();
